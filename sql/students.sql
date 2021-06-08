978d314e05cb5dff9c0e3d70758cbafcbf59a9b7
wfcsilence <65449543+wfcsilence@users.noreply.github.com>
2021-06-08 15:22:58 +0800

    second-edition

--- sql/students.sql
+++ sql/students.sql
@@ -1,6 +1,6 @@
 create table students
 (
-student_id int not null,
+student_id varchar(255) not null,
 student_name varchar(255) not null,
 student_pswd varchar(255) not null,
 class varchar(255) not null
